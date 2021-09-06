27573a09cc1b3ef5725941518f449091cdda04cc
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-09-06 13:49:05 +0430

    testing state change

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,25 @@
 import PropTypes from 'prop-types';
+import React from 'react';
 
 const Input = ({ secretWord }) => {
-  return <div data-test='component-input'></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test='component-input'>
+      <form className='form-inline'>
+        <input
+          type='text'
+          className='mb-2 mx-sm-3'
+          placeholder='Enter Guess'
+          data-test='input-box'
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button className='btn btn-primary mb-2' data-test='submit-button'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,6 +1,7 @@
 import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from './test/testUtils';
 import Input from './Input';
+import React from 'react';
 
 const secretWord = 'party';
 const setup = (props = { secretWord }) => {
@@ -16,4 +17,19 @@ test('render without error', () => {
 test('does not throw warning with expected props', () => {
   const expectedProps = { secretWord };
   checkProps(Input, expectedProps);
+});
+
+describe('test controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const event = { target: { value: 'train' } };
+    inputBox.simulate('change', event);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
 });
