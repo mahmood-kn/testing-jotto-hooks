cad1afd3bcae4a2ebd342ac4eefbb56b806c20b2
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-09-06 11:30:27 +0430

    add props to Input

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,13 +1,19 @@
 import { shallow } from 'enzyme';
-import { findByTestAttr } from './test/testUtils';
+import { findByTestAttr, checkProps } from './test/testUtils';
 import Input from './Input';
 
-const setup = () => {
-  return shallow(<Input />);
+const secretWord = 'party';
+const setup = (props = { secretWord }) => {
+  return shallow(<Input {...props} />);
 };
 
 test('render without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = { secretWord };
+  checkProps(Input, expectedProps);
 });
