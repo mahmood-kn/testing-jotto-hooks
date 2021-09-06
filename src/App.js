1fbd764433efa7393ae41c4e774112eca425f112
Mahmood Khaje Najafi <mahmood.kh.najafi@gmail.com>
2021-09-06 11:08:54 +0430

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Page1 from './components/Page1/Page1'
-import Page2 from './components/Page2/Page2'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <Page1/>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
